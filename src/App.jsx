860fc7dc32095e259ff2d020e37816ec31619207
hardope <adeyeriopeoluwa05@gmail.com>
2025-03-13 01:58:06 +0100

    hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import TrojanHorse from './pages/Trojan'
+import { useLayoutEffect } from 'react';
+
+const Wrapper = ({ children }) => {
+	const location = useLocation();
+  
+	useLayoutEffect(() => {
+	  // Scroll to the top of the page when the route changes
+	  window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+	}, [location.pathname]);
+  
+	return children;
+  };
 
 function App() {
 
 	  return (
 		<Router>
-			<Routes>
-				<Route path='/' element={<LandingPage />} />
-				<Route path='projects/trojan' element={<TrojanHorse />} />
-			</Routes>
+			<Wrapper>
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='projects/trojan' element={<TrojanHorse />} />
+				</Routes>
+			</Wrapper>
 		</Router>
 	)
 }
